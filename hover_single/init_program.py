ce88f059e9a32de512d2c572edc01e59ff0a7408
chenzhiyang <chenzhiyang@westlake.edu.cn>
2025-09-18 07:34:33 +0000

    first ver of hover

---
+++ hover_single/init_program.py
@@ -0,0 +1,15 @@
+<system_prompt_1>
+Given the fields 'claim', 'passages', produce the fields 'summary'.
+</system_prompt_1>
+
+<system_prompt_2>
+Given the fields 'claim', 'summary_1', produce the fields 'query'.
+</system_prompt_2>
+
+<system_prompt_3>
+Given the fields 'claim', 'context', 'passages', produce the fields 'summary'.
+</system_prompt_3>
+
+<system_prompt_4>
+Given the fields 'claim', 'summary_1', 'summary_2', produce the fields 'query'.
+</system_prompt_4>
